1c3b4baab2dfa03b2c39d68d46a84de31f826c88
santiago.gomez <santiago.gomez1@alumnos.uneatlantico.es>
2024-07-30 10:38:58 +0200

    Project FizzBuzz

---
+++ For/Program.cs
@@ -0,0 +1,19 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine($"iteracion {i} - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"iteracion {i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"iteracion {i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine($"iteracion {i}");
+    }
+}
